96f10550d531435bd6bf6226a0b8db0a4a36a4d2
Aidan Angus-Henry <aangushenry@ucdavis.edu>
2023-04-18 05:32:03 -0700

    load nonWF processed euro trees into nonWF sim

--- scripts/euro_trees_subset.py
+++
@@ -1,9 +0,0 @@
-import tskit
-
-euro_ts = tskit.load("../output/euro_trees.trees")
-
-print(f"The tree sequence has {euro_ts.num_trees} trees\n"
-      f"on a genome of length {euro_ts.sequence_length},\n"
-      f"{euro_ts.num_individuals} individuals, {euro_ts.num_samples} 'sample' genomes,\n"
-      f"and {euro_ts.num_mutations} mutations.")
-
